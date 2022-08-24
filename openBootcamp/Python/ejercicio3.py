728cbf888af6400fdd71fa959468baf3f6e6c257
Gaby <gaby_ag@live.com.ar>
2022-08-24 12:19:44 -0300

    ok

---
+++ openBootcamp/Python/ejercicio3.py
@@ -0,0 +1,11 @@
+#En este ejercicio tendrás que crear una variable a la cual asignarás una cadena de texto 
+#y la imprimirás por consola, a su vez, tendrás que imprimir también el tipo de la variable por consola.
+
+#Este ejercicio lo tienes que realizar desde la consola de python y subir capturas de pantalla en una carpeta comprimida zip.
+
+
+text = "Estoy aprendiendo Python en OpenBootcamp"
+
+x = type(text)
+
+print (x)
